881423475484755b8b33d158d0a242f26acc7e9b
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-10-29 14:51:02 +0900

    use hooks

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  const persons = [
-    {name: 'yuji', age: 32},
-    {name: 'yabai', age: 28},
-    {name: 'sugoi', age: 29}
-  ]
+function App(props) {
+
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      {name: 'yuji', age: 32},
+      {name: 'yabai', age: 28},
+      {name: 'sugoi', age: 29}
+    ],
+    otherState: 'some other value'
+  })
 
   const switchNameHandler = () => {
-    console.log('clicked!')
+    setPersonsState({
+      persons: [
+        {name: 'changed-yuji', age: 33},
+        {name: 'yabai', age: 28},
+        {name: 'sugoi', age: 29}
+      ]
+    })
   }
 
   return (
@@ -18,23 +28,11 @@ function App() {
       <h1>Hello world!</h1>
       <p>this is it</p>
       <button onClick={ switchNameHandler }>Switch Name</button>
-      <Person name={ persons[0].name } age={ persons[0].age }>My Hobbies: baseball</Person>
-      <Person name={ persons[1].name } />
-      <Person name={ persons[2].name } />
+      <Person name={ personsState.persons[0].name } age={ personsState.persons[0].age }>My Hobbies: baseball</Person>
+      <Person name={ personsState.persons[1].name } />
+      <Person name={ personsState.persons[2].name } />
     </div>
   );
-
-  // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hello world!!!'));
 }
-
-// class App extends Componet {
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>Hello world!</h1>
-//       </div>
-//     )
-//   }
-// }
 
 export default App;
