a0332a2534428499eef811e75cd321d7c76f0af9
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-10-29 14:26:02 +0900

    switchNameHandler

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 function App() {
+  const persons = [
+    {name: 'yuji', age: 32},
+    {name: 'yabai', age: 28},
+    {name: 'sugoi', age: 29}
+  ]
+
+  const switchNameHandler = () => {
+    console.log('clicked!')
+  }
+
   return (
     <div className="App">
       <h1>Hello world!</h1>
       <p>this is it</p>
-      <Person name="yuji" age="32">My Hobbies: baseball</Person>
-      <Person name="yabai" />
-      <Person name="sugoi"/>
+      <button onClick={ switchNameHandler }>Switch Name</button>
+      <Person name={ persons[0].name } age={ persons[0].age }>My Hobbies: baseball</Person>
+      <Person name={ persons[1].name } />
+      <Person name={ persons[2].name } />
     </div>
   );
 
