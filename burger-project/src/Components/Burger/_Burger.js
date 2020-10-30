829bf86efc3e22e5e66c207c2216b4d35aab5462
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-10-30 21:44:36 +0900

    sum dynamically

--- burger-project/src/Components/Burger/_Burger.js
+++ burger-project/src/Components/Burger/_Burger.js
@@ -4,13 +4,22 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    // オブジェクトのキーで値を一つずつ取り出し、[[オブジェクトの値]...]
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      // 問題は繰り返す回数
-      return <BurgerIngredient key={ igKey + i } type={ igKey } />
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      // オブジェクトのキーで値を一つずつ取り出し、[[オブジェクトの値]...]
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        // 問題は繰り返す回数
+        return <BurgerIngredient key={ igKey + i } type={ igKey } />
+      })
     })
-  });
+    .reduce((arr, el) => {
+      // 既存の配列を組み合わせ、新たな配列を生成
+      return arr.concat(el)
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
 
   return (
     <div className={ classes.Burger }>
