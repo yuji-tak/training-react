658e609556eec7cfd813620502aa7e65cdabb999
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-10-31 12:39:01 +0900

    build control

--- burger-project/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ burger-project/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Burger from '../../Components/Burger/_Burger';
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
   state = {
@@ -16,6 +17,7 @@ class BurgerBuilder extends Component {
     return (
       <>
         <Burger ingredients={ this.state.ingredients } />
+        <BuildControls />
       </>
     );
   }
