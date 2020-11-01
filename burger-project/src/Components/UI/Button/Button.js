dbdd6cfff042b49a51fe535f1a7f9dd196dd4f96
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-01 11:35:16 +0900

    button component

--- burger-project/src/Components/UI/Button/Button.js
+++ burger-project/src/Components/UI/Button/Button.js
@@ -3,9 +3,10 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const button = (props) => (
-<button
-  className={ [ classes.Button, classed[ props.btnType ].join(' ') ] }
-  onClick={ props.clicked }>{ props.children }</button>
+  // joinは2つのクラス名を分離している
+  <button
+    className={ [ classes.Button, classes[ props.btnType ] ].join(' ') }
+    onClick={ props.clicked }>{ props.children }</button>
 );
 
 export default button;
