c9d4781c7fc32672a158f136235a4aed4a31ebcd
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-01 14:33:04 +0900

    create navigation

--- burger-project/src/Components/Burger/BuildControls/BuildControls.js
+++ burger-project/src/Components/Burger/BuildControls/BuildControls.js
@@ -4,10 +4,10 @@ import classes from './BuildControls.module.css';
 import BuildControl from './BuildControl/BuildControl'
 
 const controls = [
-  { label: 'Salad', type: 'salad' },
-  { label: 'Bacon', type: 'bacon' },
-  { label: 'Cheese', type: 'cheese' },
-  { label: 'Meat', type: 'meat' },
+  { label: '🥑 Salad', type: 'salad' },
+  { label: '🥓 Bacon', type: 'bacon' },
+  { label: '🧀 Cheese', type: 'cheese' },
+  { label: '🍖 Meat', type: 'meat' },
 ]
 
 const buildControls = (props) => (
