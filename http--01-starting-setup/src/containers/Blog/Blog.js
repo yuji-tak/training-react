b86de6beb4a4e418995b097ccb4b157f6a1a2b54
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-02 15:25:15 +0900

    making a post selectable

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -5,24 +5,41 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import post from '../../components/Post/Post';
 
 class Blog extends Component {
 
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   }
 
   componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(res => {
-        this.setState({ posts: res.data })
+        const posts = res.data.slice(0, 4);
+        const updatePosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Yuji'
+          }
+        })
+        this.setState({ posts: updatePosts })
       });
+  }
 
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
   }
 
     render () {
+      // postsの値のデータ型が配列にしとかないとmap()が使えない
       const posts = this.state.posts.map(post => {
-        return <Post title={ post.title } />
+        return <Post
+          key={ post.id }
+          title={ post.title }
+          author={ post.author }
+          clicked={ () => this.postSelectedHandler(post.id) } />
       })
 
         return (
@@ -31,7 +48,7 @@ class Blog extends Component {
                     { posts }
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={ this.state.selectedPostId } />
                 </section>
                 <section>
                     <NewPost />
