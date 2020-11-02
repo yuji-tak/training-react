7ed4140b9c3c6977dcecac1c4e1d441b6f0afca9
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-02 11:53:59 +0900

    _modal classへ

--- burger-project/src/Components/Burger/OrderSummary/OrderSummary.js
+++ burger-project/src/Components/Burger/OrderSummary/OrderSummary.js
@@ -5,7 +5,7 @@ import Button from '../../UI/Button/Button';
 class OrderSummary extends Component {
 
   componentWillUpdate() {
-    console.log('hi');
+    console.log('[ordersummary] hi');
   }
 
   render() {
--- burger-project/src/Components/UI/modal/_Modal.js
+++ burger-project/src/Components/UI/modal/_Modal.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './_Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <>
-    <Backdrop show={ props.show } clicked={ props.modalClosed } />
-    <div
-      className={ classes.Modal }
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-      { props.children }
-    </div>
-  </>
-);
+class Modal extends Component {
 
-export default modal;
+  // 何を検証してるのか？？？
+  // showが変化した時だけ、このコンポーネントが呼ばれるようにしている？
+  shouldComponentUpdate ( nextProps, nextState ) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate () {
+    console.log('[modal] willupdate')
+  }
+
+  render() {
+    return (
+      <>
+        <Backdrop show={ this.props.show } clicked={ this.props.modalClosed } />
+        <div
+          className={ classes.Modal }
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          { this.props.children }
+        </div>
+      </>
+    );
+  }
+}
+
+export default Modal;
