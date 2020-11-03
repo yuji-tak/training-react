848e2318bc0afd02c8fe43ec66a35349b75b5d4f
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-03 21:16:59 +0900

    active class

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts'
@@ -14,12 +14,19 @@ class Blog extends Component {
           <header>
             <nav>
               <ul>
-                <li><Link to="/">HOME</Link></li>
-                <li><Link to={{
+                <li><NavLink
+                  to="/"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: '#fa923f',
+                    textDecoration: 'underline'
+                  }}>HOME</NavLink></li>
+                <li><NavLink to={{
                   pathname: '/new-post',
                   hash: '#submit',
                   search: '?quick-submit=true'
-                }}>New Post</Link></li>
+                }}>New Post</NavLink></li>
               </ul>
             </nav>
           </header>
