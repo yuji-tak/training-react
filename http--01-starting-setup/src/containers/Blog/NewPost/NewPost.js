7903a93062ed330d53971e212f288f557263efc9
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-03 19:28:42 +0900

    modify

--- http--01-starting-setup/src/containers/Blog/NewPost/NewPost.js
+++ http--01-starting-setup/src/containers/Blog/NewPost/NewPost.js
@@ -4,11 +4,11 @@ import axios from 'axios';
 import './NewPost.css';
 
 class NewPost extends Component {
-    state = {
-        title: '',
-        content: '',
-        author: 'Max'
-    }
+  state = {
+    title: '',
+    content: '',
+    author: 'Max'
+  }
 
   postDataHandler = () => {
     const data = {
@@ -24,24 +24,24 @@ class NewPost extends Component {
       });
   }
 
-    render () {
-        return (
-            <div className="NewPost">
-                <h1>Add a Post</h1>
-                <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
-                <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
-                <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
-                </select>
-                {/* アロー関数にして、押下時のみ関数を実行する制御が必要ではないのか */}
-                <button onClick={ this.postDataHandler }>Add Post</button>
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div className="NewPost">
+        <h1>Add a Post</h1>
+        <label>Title</label>
+        <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+        <label>Content</label>
+        <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+        <label>Author</label>
+        <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+            <option value="Max">Max</option>
+            <option value="Manu">Manu</option>
+        </select>
+        {/* アロー関数にして、押下時のみ関数を実行する制御が必要ではないのか */}
+        <button onClick={ this.postDataHandler }>Add Post</button>
+      </div>
+    );
+  }
 }
 
 export default NewPost;
