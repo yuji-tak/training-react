ae0f12f2d324b07b5a7ea8f6b2bfca95bad2020f
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-03 19:35:42 +0900

    using link

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts'
@@ -14,8 +14,12 @@ class Blog extends Component {
           <header>
             <nav>
               <ul>
-                <li><a href="/">HOME</a></li>
-                <li><a href="/new-post">New Post</a></li>
+                <li><Link to="/">HOME</Link></li>
+                <li><Link to={{
+                  pathname: '/new-post',
+                  hash: '#submit',
+                  search: '?quick-submit=true'
+                }}>New Post</Link></li>
               </ul>
             </nav>
           </header>
