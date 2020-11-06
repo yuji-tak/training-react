fc247f21b624f218d1df6c956d6a9bf29a2d5897
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-06 15:54:59 +0900

    props.historyなど

--- burger-project/src/Containers/Checkout/Checkout.js
+++ burger-project/src/Containers/Checkout/Checkout.js
@@ -6,28 +6,32 @@ import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      meat: 1,
-      cheese: 1,
-      bacon: 1
-    }
+    ingredients: null,
+    price: 0
   }
 
-  componentDidMount () {
+  componentWillMount () {
     // URLSearchParamsオブジェクトを返すコンストラクター
     // URLSearchParams.entries()このオブジェクトに含まれる全てのキーと値のペアを列挙するためのiteratorを返す
     // ?bacon=0&cheese=0&meat=1&salad=0
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
+    let price = 0;
+
     // Object.entries()で配列形式へ変換
     for ( let param of query.entries()) {
       // ここでparamのデータは['salad', '1']の形になる
       // +はNumber型へ変換
       // ingredientsオブジェクトへプロパティと値を代入
-      ingredients[param[0]] = +param[1];
+
+      if (param[0] === 'price') {
+        price = param[1];
+      } else {
+        ingredients[param[0]] = +param[1];
+      }
+
     }
-    this.setState({ ingredients: ingredients });
+    this.setState({ ingredients: ingredients, totalPrice: price });
   }
 
   // class内でこのリテラルを用いるとthisが使える
@@ -47,7 +51,10 @@ class Checkout extends Component {
           checktoutCancelled={ this.checktoutCancelledHandler }
           checkoutContinued={ this.checktoutContinuedHandler }/>
         <Route
-          path={ this.props.match.path + '/contact-data' } component={ ContactData } />
+          path={ this.props.match.path + '/contact-data' }
+          // 引数を渡す為にcomponentではなくrenderで記述している？
+          // 引数propsと属性に{...}展開で、子コンポーネントへprops.historyオブジェクトを渡している？
+          render={ (props) => (<ContactData ingredients={ this.state.ingredients } price={ this.state.totalPrice } { ...props } />) } />
       </>
     );
   }
