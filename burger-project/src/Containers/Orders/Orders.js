a856e13e0632ad82693e5d781d922ac9f57534a0
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-06 17:36:38 +0900

    fetching orders

--- burger-project/src/Containers/Orders/Orders.js
+++ burger-project/src/Containers/Orders/Orders.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 
 import Order from '../../Components/Order/Order';
+import axios from '../../_axios-order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  }
+
+  componentDidMount() {
+    axios.get('/orders.json')
+      .then(res => {
+        const fetchedOrders = [];
+        // firebaseのドキュメント？idをkeyに中のオブジェクトを配列へ格納し、スプレッド演算子を用いつつ新たにidプロパティを作成し、keyを格納
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          });
+        }
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch(err => {
+        // ここに下記記述いるのはなぜ？
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <>
@@ -13,4 +39,4 @@ class Orders extends Component {
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
