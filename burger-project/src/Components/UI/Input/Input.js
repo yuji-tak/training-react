343c4768e4c514b4a3c56595167ea04d734e9c32
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-07 16:28:39 +0900

    create select

--- burger-project/src/Components/UI/Input/Input.js
+++ burger-project/src/Components/UI/Input/Input.js
@@ -20,6 +20,20 @@ const input = (props) => {
         { ...props.elementConfig }
         value={ props.value } />;
       break;
+    case ( 'select' ):
+      inputElement = (
+        <select
+          className={ classes.InputElement }
+          { ...props.elementConfig }
+          value={ props.value }>
+            {props.elementConfig.options.map(option => (
+              <option key={ option.value } value={ option.value }>
+                { option.displayValue }
+              </option>
+            ))}
+        </select>
+      );
+      break;
     default:
       inputElement = <input
         className={ classes.InputElement }
