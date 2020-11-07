aff26aca97c1462633f9877109cdba94b32ee352
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-07 16:14:55 +0900

    dynamically create inputs

--- burger-project/src/Containers/Checkout/ContactData/ContactData.js
+++ burger-project/src/Containers/Checkout/ContactData/ContactData.js
@@ -84,12 +84,24 @@ class ContactData extends Component {
   }
 
   render () {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
+
     let form = (
       <form>
-        <Input inputtype="input" type="text" name="name" placeholder="Your Name"/>
-        <Input inputtype="input" type="email" name="email" placeholder="Your Mail"/>
-        <Input inputtype="input" type="text" name="street" placeholder="Your Street"/>
-        <Input inputtype="input" type="text" name="postal" placeholder="Postal Code"/>
+        {/* map内は{}ではなく、() */}
+        { formElementsArray.map(formElement => (
+          <Input
+            key={ formElement.id }
+            elementType={ formElement.config.elementType }
+            elementConfig={ formElement.config.elementConfig }
+            value={ formElement.config.value } />
+        )) }
         <Button
           btnType="Success"
           clicked={ this.orderHandler }>ORDER</Button>
