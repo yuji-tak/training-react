41ae5daa2910b901e0ff1e48cf0baf6ae9a9c574
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-07 15:06:08 +0900

    input component

---
+++ burger-project/src/Components/UI/Input/Input.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import classes from './Input.module.css';
+
+const input = (props) => {
+  let inputElement = null;
+
+  switch (props.inputType) {
+    case ( 'input' ):
+      // スプレッド演算子で展開しつつ渡す理由は？
+      inputElement = <input className={ classes.InputElement } { ...props } />;
+      break;
+    case ( 'textarea' ):
+      inputElement = <textarea className={ classes.InputElement } { ...props } />;
+      break;
+    default:
+      inputElement = <input className={ classes.InputElement } { ...props } />
+  }
+
+  return (
+    <div className={ classes.Input }>
+      <label className={ classes.Label }>{ props.label }</label>
+      { inputElement }
+    </div>
+  );
+
+};
+
+export default input;
