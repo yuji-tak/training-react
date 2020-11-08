e6328dc6c09cf47f71c9334437ca73c09955319a
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-08 19:50:49 +0900

    reducer & store

---
+++ redux--01-start/redux-basics.js
@@ -0,0 +1,20 @@
+// Common JSスタイル何だね
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+  counter: 0
+}
+
+// Reducer
+const rootReducer = (state = initialState, action) => {
+  return state;
+}
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// Dispatching Action
+
+// Subscription
