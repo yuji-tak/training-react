af6e18d82f722b72043ef18d3c4efbde89c37bc5
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-12 14:59:49 +0900

    switch

--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -3,27 +3,26 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-  if ( action.type === 'INCREMENT' ) {
-    return {
-      counter: state.counter + 1
-    }
+  switch ( action.type ) {
+    case 'INCREMENT':
+      // returnがある場合はbreakが不要
+      return {
+        counter: state.counter + 1
+      }
+    case 'DECREMENT':
+      return {
+        counter: state.counter - 1
+      }
+    case 'ADD':
+      return {
+        counter: state.counter + action.val
+      }
+    case 'SUBTRACT':
+      return {
+        counter: state.counter - action.val
+      }
   }
-  if ( action.type === 'DECREMENT' ) {
-    return {
-      counter: state.counter - 1
-    }
-  }
-  if ( action.type === 'ADD') {
-    return {
-      counter: state.counter + action.val
-    }
-  }
-  if ( action.type === 'SUBTRACT' ) {
-    return {
-      counter: state.counter - action.val
-    }
-  }
-
+  // このreturnがある為に、defaultが不要？
   return state;
 };
 
