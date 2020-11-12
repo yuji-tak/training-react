89001a42d211f269d1a2ff528b292dff481c6f30
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-12 16:42:16 +0900

    updating state immutably

--- redux--01-start/src/containers/Counter/Counter.js
+++ redux--01-start/src/containers/Counter/Counter.js
@@ -13,6 +13,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
+                <hr />
+                <button onClick={ this.props.onStoreResult }>Store Result</button>
+                <ul>
+                  {this.props.storedResults.map(strResult => (
+                  <li onClick={ this.props.onDeleteResult } key={strResult.id}>{ strResult.value }</li>
+                  ))}
+                </ul>
             </div>
         );
     }
@@ -20,7 +27,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
   return {
-    ctr: state.counter
+    ctr: state.counter,
+    storedResults: state.results
   };
 };
 
@@ -30,6 +38,8 @@ const mapDispatchToProps = dispatch => {
     onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
     onAddCounter: () => dispatch({ type: 'ADD', val: 10 }),
     onSubtractCounter: () => dispatch({ type: 'SUBTRACT', val: 15 }),
+    onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
+    onDeleteResult: () => dispatch({ type: 'DELETE_RESULT' })
   };
 };
 
--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -1,25 +1,43 @@
+import { Provider } from "react-redux";
+
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 }
 
 const reducer = (state = initialState, action) => {
   switch ( action.type ) {
     case 'INCREMENT':
+      // 元のstateは不変のまま、値をコピー？
+      const newState = Object.assign({}, state);
+      newState.counter = state.counter;
       // returnがある場合はbreakが不要
-      return {
-        counter: state.counter + 1
-      }
+      return newState;
     case 'DECREMENT':
       return {
+        // 引数のstateを展開しながら、counterプロパティの値を変更
+        ...state,
         counter: state.counter - 1
       }
     case 'ADD':
       return {
+        ...state,
         counter: state.counter + action.val
       }
     case 'SUBTRACT':
       return {
+        ...state,
         counter: state.counter - action.val
+      }
+    case 'STORE_RESULT':
+      return {
+        ...state,
+        results: state.results.concat({ id: new Date(),value: state.counter })
+      }
+    case 'DELETE_RESULT':
+      return {
+        ...state,
+        results: state.results.splice(0, 1)
       }
   }
   // このreturnがある為に、defaultが不要？
