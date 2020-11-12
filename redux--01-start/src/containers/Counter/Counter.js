5f9236c58cb8599597690c6f1def80c67830bedd
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-12 14:14:19 +0900

    dispatching actions from within the component3

--- redux--01-start/src/containers/Counter/Counter.js
+++ redux--01-start/src/containers/Counter/Counter.js
@@ -5,35 +5,14 @@ import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    // state = {
-    //     counter: 0
-    // }
-    // counterChangedHandler = ( action, value ) => {
-    //     switch ( action ) {
-    //         case 'inc':
-    //             this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-    //             break;
-    //         case 'dec':
-    //             this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-    //             break;
-    //         case 'add':
-    //             this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-    //             break;
-    //         case 'sub':
-    //             this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-    //             break;
-    //         default:
-    //     }
-    // }
-
     render () {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={() => this.props.onIncrementCounter( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.props.onDecrementCounter( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.props.onAddCounter( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter( 'sub', 5 )}  />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
             </div>
         );
     }
