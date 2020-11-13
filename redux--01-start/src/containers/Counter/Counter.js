5340391c8ec56a8e02a2e417632e069620d33011
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-11-13 15:45:17 +0900

    combining multiple reducers2

--- redux--01-start/src/containers/Counter/Counter.js
+++ redux--01-start/src/containers/Counter/Counter.js
@@ -15,7 +15,7 @@ class Counter extends Component {
                 <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
                 <hr />
-                <button onClick={ this.props.onStoreResult }>Store Result</button>
+                <button onClick={ () => this.props.onStoreResult(this.props.ctr) }>Store Result</button>
                 <ul>
                   {this.props.storedResults.map(strResult => (
                   <li onClick={ () => this.props.onDeleteResult(strResult.id) } key={strResult.id}>{ strResult.value }</li>
@@ -39,7 +39,7 @@ const mapDispatchToProps = dispatch => {
     onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
     onAddCounter: () => dispatch({ type: actionTypes.ADD, val: 10 }),
     onSubtractCounter: () => dispatch({ type: actionTypes.SUBTRACT, val: 15 }),
-    onStoreResult: () => dispatch({ type: actionTypes.STORE_RESULT }),
+    onStoreResult: (result) => dispatch({ type: actionTypes.STORE_RESULT, result: result }),
     onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, resultElementId: id })
   };
 };
